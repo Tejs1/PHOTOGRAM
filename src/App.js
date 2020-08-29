793a75d7e14144d54d91d649c4adc1349b3c44eb
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2020-08-30 00:24:05 +0530

    skeleton done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Post from './Post';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts,setposts] = useState
+    ([
+    {
+        username: "kartik",
+        caption: "this works",
+        imageUrl: "https://reactjs.org/logo-og.png" 
+    },
+    {
+        username: "kartik",
+        caption: "this works",
+        imageUrl: "https://reactjs.org/logo-og.png"
+    }
+]);
+    return (
+        <div className ="app">
+            {/* HEADER */}
+            <div className="app__header">
+                <img
+                    className="app__headerImage"
+                    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                    alt=""
+                />
+            </div>
+            
+            <h1>HI there whats up lets start </h1>
+            {/* POST */}
+            {
+                posts.map(post => (
+                    <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+                ))
+            }
+        </div>
+    );
 }
 
 export default App;
