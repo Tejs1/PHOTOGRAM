89f55bea4db889aa0f23e0ef932febf5f6befa92
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2020-08-31 20:23:29 +0530

    user auth and singup window

--- src/App.js
+++ src/App.js
@@ -1,38 +1,141 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
 import Post from './Post';
-import {db} from './firebase';
+import {db, auth} from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 
 function App() {
-    const [posts, setPosts] = useState([]);
-//  useEffect ->runs a piece of code based on a specific condition   
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
     
+    const [posts, setPosts] = useState([]);
+    const [open,setOpen] =useState(false);
+    const [username,setUsername] =useState('');
+    const [password,setPassword] =useState('');
+    const [email,setEmail] =useState('');
+    const [user,setUser] =useState(null);
     useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                //user has logged in...
+                console.log(authUser);
+                setUser(authUser);
+
+                if (authUser.displayName) {
+                    // dont update username
+                } else {
+                    // if we just created someone
+                    return authUser.updateProfile({
+                        displayName: username,
+                    });
+                }
+            } else {
+                //user has logged out...
+                setUser(null);
+            }
+        })
+
+        return () => {
+            // perform some cleanup actions
+            unsubscribe();
+        }
+    }, [user, username]);
+    //  useEffect ->runs a piece of code based on a specific condition   
+    useEffect(() =>{
     // this is where code runs
         db.collection('posts').onSnapshot(snapshot => {
         // every time new post is added, this code fires up
-        setPosts(snapshot.docs.map(doc => doc.data()));
+        setPosts(snapshot.docs.map(doc => ({
+            id: doc.id,
+            post: doc.data()
+        })));
     })
+}, []);
     //If present, effect will only activate if the values in the list change.
-    }, []);
+    
+    const signUp =(event) => {
+        event.preventDefault();
+        auth.createUserWithEmailAndPassword(email, password)
+        .catch((error) => alert(error.message));
+    }
 
     return (
         <div className ="app">
-            {/* HEADER */}
-            <div className="app__header">
-                <img
+        <Modal
+            open={open}
+            onClose={() => setOpen(false)}
+        >
+            <div style={modalStyle} className={classes.paper}>
+             <form className="app__signup">
+                <center>
+                    <img
                     className="app__headerImage"
                     src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
                     alt=""
-                />
+                    /> 
+                </center> 
+                    <Input 
+                        placeholder="username"
+                        type="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input 
+                        placeholder="email"
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input 
+                        placeholder="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button onClick={signUp}>Sign Up</Button>
+             </form>
             </div>
-            
+        </Modal>
+
+            {/* HEADER */}
+            <div className="app__header">
+                    <img
+                    className="app__headerImage"
+                    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                    alt=""
+                    />
+            </div>
+            <Button onClick={()=> setOpen(true)}>Sign Up</Button>
             <h1>HI there whats up lets start </h1>
             {/* POST */}
             {
-                posts.map(post => (
-                    <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+                posts.map(({id, post}) => (
+                    <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
                 ))
             }
         </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,15 @@
 import firebase from "firebase"
 
-// const firebaseApp = firebase.initializeApp({
-//   apiKey: "AIzaSyCX0ShsW1PzTDyLYqBm7EPTgOsknicdUQg",
-//   authDomain: "instaguram.firebaseapp.com",
-//   databaseURL: "https://instaguram.firebaseio.com",
-//   projectId: "instaguram",
-//   storageBucket: "instaguram.appspot.com",
-//   messagingSenderId: "515841607846",
-//   appId: "1:515841607846:web:47732c6c1ef30cb7314c67",
-//   measurementId: "G-41794C202E"
-// });
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyADTIPKHgk31lti48-kyLdFwdt8f7SmHwc",
-  authDomain: "banded-pad-282116.firebaseapp.com",
-  databaseURL: "https://banded-pad-282116.firebaseio.com",
-  projectId: "banded-pad-282116",
-  storageBucket: "banded-pad-282116.appspot.com",
-  messagingSenderId: "723493373017",
-  appId: "1:723493373017:web:e61ec9b2051653197a1d48",
-  measurementId: "G-MB7HRJEZ3R"
+const firebaseApp = firebase.initializeApp ({
+  apiKey: "AIzaSyC-D9Pib4RzYAkCQK-TfMp20hGHWA4guTk",
+  authDomain: "insta-e6a3c.firebaseapp.com",
+  databaseURL: "https://insta-e6a3c.firebaseio.com",
+  projectId: "insta-e6a3c",
+  storageBucket: "insta-e6a3c.appspot.com",
+  messagingSenderId: "99667606782",
+  appId: "1:99667606782:web:1833acf8a1387c6211b9af",
+  measurementId: "G-QRN9M76LCK"
 });
-
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
